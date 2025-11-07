9ae4ca8f8d67d7e1162226789814a91fe39f3bd1
Bagus <bagustris@outlook.com>
2025-11-08 06:57:09 +0900

    add breast cancer

---
+++ lessons/5-HandsOn/breast_cancer.py
@@ -0,0 +1,40 @@
+# breast_cancer.py
+# Reference: https://www.geeksforgeeks.org/machine-learning/ml-cancer-cell-classification-using-scikit-learn/
+
+# 0. import library
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+data = load_breast_cancer()
+
+# Exploring the dataset with Pandas
+df=pd.DataFrame(data.data,columns=data.feature_names)
+df.sample(5)
+
+df.info()
+df.describe()
+
+# Analyze data.target to understand the distribution of malignant and benign cases as class imbalance can affect model performance.
+df2=pd.DataFrame(data.target,columns=['target'])
+df2.sample(5)
+
+class_counts=df2["target"].value_counts()
+plt.pie(class_counts, labels=class_counts.index, autopct='%1.2f%%', colors=['red', 'green'])
+
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33, random_state=42)
+
+# Train the Gaussian Naive Bayes model
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
